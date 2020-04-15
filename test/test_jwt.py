8242c5864b601379fff7d8953a6798a1fae5943c
Pablo Álvarez Córdoba <pabloalvarezcordoba@protonmail.ch>
2020-04-15 13:48:56 +0200

    tests: add test case for invalid JWT

--- test/test_jwt.py
+++ test/test_jwt.py
@@ -59,10 +59,17 @@ class TestJWT(unittest2.TestCase):
         time.sleep(2)
 
     def test_auth(self):
-        """Test if we can log in with a JWT
-
-        :return: None
+        """
+        Test if we can log in with a JWT
         """
         token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VybmFtZSI6ImFkbWluIn0.WKnS2svWExnBoQNnv8LvCk6Fcoqt5hBgYMV3rqbfLNDr4fiPEJKG8Vv9w0N8UbvDh4glnrIBhLNstb_F8IWoYI2ZUKY2LEAPkXkCwMTMab2Yi9-QKW6LMvNfP_-rq409e6Lk4No6WPRLixivzb7FcxuwK13nkrQERHwTYeatoOW_sAVAtaP9DtJF1jYTbHsEVSbbRWXTVM50x1bu6m-faxF8xENY5lAZaUF3949oIm9wkJKtYL6VyvrklsQ91hLeKipnxN9fkru_REhjGpDM211ZhtIkQ9RPakGP7ktKhvo0O-XXZdVOTmyLrdwLMkl9aP5x2by6ibCbhGCJMSwsYLPlqUqL7kZ0LD_UdMJCql4h-OeK-RWGdeVLm1qhQC8BmfR-QOP3dNjkcDzTvWMyYVKbR4jH2Y1BjzI6t8yr8aFGhhuRdPhAzJ_NzWbujgiamwW5K_10qJt3jZ-sewmUBsbAwxcTyn5XPypknZeGUt5kmK948wZyO4NVTvKZzamwIsnRZe_KoNC3sz1lj0zU8gyo-3r2MPh9uNTphdxrKEvavAxYpTyy6qASHSZ-Hdg-_59HjHuYfCWtBRXSHz_5dgWlI3nKbzB-064Zqag-vYNde2XaxlWmdTkoyvWl8qDpJ6T8zpNLFktndjlf6Fq93adZZQw4VLFdVELLY1_xMZ4"
         headers = {'Authorization': 'Bearer {}'.format(token)}
         assert requests.get(self.endpoint, headers=headers).status_code == 200
+
+    def test_bad_token(self):
+        """
+        Test that we can't login with an invalid token
+        """
+        token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIn0.clWhshrjJR6IGncWNbBaUlzjsCRjN-Jll406ZJmE3wWt9QCsyeSQQYpVWOY0bHZOl4EydC8ko2SY0SYpe9vayx9AL9hSncoylYfdclwJDtmT3Y8Y9aFkJuX4yZjo6MDg7YUsYbvRiBlQucZwCsPC2nraFFhYC3u0E3niHfOZKIOgk_5TpMUoUst_Jvyu3eASomMAs0jiuFHWkbVogXiYwfQThWQzJOXhGx9H-8KPhLJCuD5fpRW1IW5ML_AQKrBcWLjDo3A4GZFwVZNi43XkK1BgHw9stcPPdYepYxLm_4_F6aJGO7SUWlHnqVa89RPcaNpo3TLhQOW01dtrH1khTA"
+        headers = {'Authorization': 'Bearer {}'.format(token)}
+        assert requests.get(self.endpoint, headers=headers).status_code == 401
